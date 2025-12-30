f7f9c98856822edfaff496fbcbf28d336001bfb9
zakariyae elmahjoubi <zakariyae.elmahjoubi.d23@ump.ac.ma>
2025-12-30 20:51:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 self.addEventListener('install', function(e) {
  console.log('[ServiceWorker] Install');
 });
-
 self.addEventListener('fetch', function(e) {
- // هذا الكود فارغ عمداً، وظيفته فقط تفعيل خاصية التثبيت على الهاتف
 });
